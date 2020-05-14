ba1de6741b9e86c570a98880d4de6dc65ce503c7
davidkcode <davidk.codedev@gmail.com>
2020-05-14 18:45:26 +0200

    Customizing profile.html

--- image-gallery/js/profile.js
+++ image-gallery/js/profile.js
@@ -1,4 +1,5 @@
 const db = firebase.firestore();
+const container = document.querySelector(".personalInfoContainer");
 
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
@@ -7,9 +8,7 @@ firebase.auth().onAuthStateChanged(function (user) {
       .get()
       .then((doc) => {
         if (doc.exists) {
-          console.log(doc.data());
-        } else {
-          console.log(doc);
+          createMarkup(doc.data());
         }
       })
       .catch((err) => {
@@ -17,3 +16,17 @@ firebase.auth().onAuthStateChanged(function (user) {
       });
   }
 });
+
+const createMarkup = (data) => {
+  let firstname = document.createElement("p");
+  let lastname = document.createElement("p");
+  let email = document.createElement("p");
+
+  firstname.innerHTML = "Firstname: " + data.firstname;
+  lastname.innerHTML = "Lastname: " + data.lastname;
+  email.innerHTML = "Email: " + data.email;
+
+  container.appendChild(firstname);
+  container.appendChild(lastname);
+  container.appendChild(email);
+};
